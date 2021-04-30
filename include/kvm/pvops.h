0fd1c3ae61c849efc8547921b8115c38fbf25ca0
Sheel Pethe <sheel.pethe@gmail.com>
2021-04-29 20:01:33 -0400

    Wrapping pvops header under CONFIG_RISCV

--- include/kvm/pvops.h
+++ include/kvm/pvops.h
@@ -1,9 +1,17 @@
 #ifndef __KVM_PVOPS_H__
 #define __KVM_PVOPS_H__
 
-#include <asm/kvm_host.h>
+#ifdef CONFIG_RISCV
 
+#include <asm/kvm_host.h>
 #define kvm_pvops(...) kvm_call_core(__VA_ARGS__)
+
+#else
+
+extern u64 __kvm_call_hyp(void *hypfn, ...);
+#define kvm_pvops(call_num, ...) __kvm_call_hyp(call_num, ##__VA_ARGS__)
+
+#endif
 
 #define KVM_SET_DESC_PFN 0x81000
 #define KVM_UNSET_DESC_PFN 0x82000
