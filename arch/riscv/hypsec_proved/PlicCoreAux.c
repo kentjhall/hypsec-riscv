d31f17a3be0589cdd2f96993cd7c354ada436bef
Kent Hall <kent@kentjhall3@hotmail.com>
2021-05-02 01:47:06 -0400

    writel (for PLIC) was faulting in HS with SMP fsr, changed to readl_relaxed/writel_relaxed

--- arch/riscv/hypsec_proved/PlicCoreAux.c
+++ arch/riscv/hypsec_proved/PlicCoreAux.c
@@ -9,7 +9,7 @@ void __handle_plic_write(u64 fault_ipa, u32 len, unsigned long insn)
 	if(len == 4)
 	{
 		data = host_get_mmio_data(insn);
-		writel((u32)data, base);
+		writel_relaxed((u32)data, base);
 	}
 	else
 	{
@@ -29,7 +29,7 @@ void __handle_plic_read(u64 fault_ipa, u32 len, unsigned long insn)
 	shift = host_dabt_get_shift(insn, len);
 	if (len == 4)
 	{
-		data = (u64)readl(__va(fault_ipa));
+		data = (u64)readl_relaxed(__va(fault_ipa));
 		set_host_regs(rt, (ulong)data << shift >> shift);
 	}
 	else
